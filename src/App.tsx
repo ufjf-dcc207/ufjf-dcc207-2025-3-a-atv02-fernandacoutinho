d21cc2019b29bc00a8bdf05129ede800b9ff46f7
Fernanda Coutinho <fernanda.coutinho@estudante.ufjf.br>
2025-09-23 22:14:58 -0300

    :zap: cria componente

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import './App.css'
+import Disciplina from './Disciplina';
 
 function App() {
   return (
     <>
       <h1>Olá Mundo!</h1>
       <h2>Olá react!</h2>
+      <Disciplina/>
     </>
   ); 
 }
---
+++ src/Disciplina.tsx
@@ -0,0 +1,6 @@
+export default function Disciplina(){
+        return <>
+            <h1>DCC207</h1>
+            <h2>Desenvolvimento Web Front End</h2>
+        </>
+}
